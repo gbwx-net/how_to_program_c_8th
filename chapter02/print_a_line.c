55bdb2dd714cf4dcd81fd80f75c8025b7e434bef
Graham <s3242096@student.rmit.edu.au>
2019-10-31 09:04:20 +0800

    Create print_a_line.c
    First program ch02

---
+++ chapter02/print_a_line.c
@@ -0,0 +1,10 @@
+/* A first program in C. */
+#include <stdio.h>
+
+/* Function main begins program execution like in Java */
+int main( void )
+{
+    printf( "Welcome to C!\n" );
+
+    return 0; /* Not in book, but required */
+}
