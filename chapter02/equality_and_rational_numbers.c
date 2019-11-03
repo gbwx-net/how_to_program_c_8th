8f445bbc56ffac60689a3ee08c0302cb8405145f
Graham <s3242096@student.rmit.edu.au>
2019-11-03 11:23:45 +0800

    Create equality_and_rational_numbers.c
    Using if statements, relational operators, and equality operators.

---
+++ chapter02/equality_and_rational_numbers.c
@@ -0,0 +1,49 @@
+/* Using if statements, relational operators, and equality operators. */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* function main start */
+int main ( void )
+{
+    int num1, num2; /* define number vars */
+
+    printf( "Enter two integers (num1 num2),\n" ); /* prompt the user for input */
+    printf( "and I will tell you the relationships they satisfy: " );
+    
+    scanf( "%d %d", &num1, &num2 ); /* assign input to vars */
+
+    /* logic checks */
+    if ( num1 == num2 )
+    {
+        printf( "%d is equal to %d\n", num1, num2 );
+    }
+
+    if ( num1 != num2 )
+    {
+        printf( "%d is not equal to %d\n", num1, num2 );
+    }
+
+    if ( num1 < num2 )
+    {
+        printf( "%d is less than %d\n", num1, num2 );
+    }
+
+    if ( num1 > num2 )
+    {
+        printf( "%d greater than %d\n", num1, num2 );
+    }
+
+    if ( num1 <= num2 )
+    {
+        printf( "%d is less than or equal to %d\n", num1, num2 );
+    }
+
+    if ( num1 >= num2 )
+    {
+        printf( "%d is greater than or equal to %d\n", num1, num2 );
+    }
+
+    return EXIT_SUCCESS; /* C90 compliance */
+} 
+/* function main end */
