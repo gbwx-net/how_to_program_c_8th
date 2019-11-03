1b2f1625d29e344237dfe112b40573a372c5f6e4
Graham <s3242096@student.rmit.edu.au>
2019-11-03 11:11:31 +0800

    Update print_a_line.c
    Added return EXIT_SUCCESS; for C90 compat

--- chapter02/print_a_line.c
+++ chapter02/print_a_line.c
@@ -1,10 +1,11 @@
 /* A first program in C. */
 #include <stdio.h>
+#include <stdlib.h>
 
 /* Function main begins program execution like in Java */
 int main( void )
 {
     printf( "Welcome to C!\n" );
 
-    return 0; /* Not in book, but required */
+    return EXIT_SUCCESS; /* Not in book, but required */
 } /* End main */
